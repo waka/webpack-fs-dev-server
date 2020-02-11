b4ddeb1904b3e30f8550f403afbd4ebb270d3c3f
yo_waka <y.wakahara@gmail.com>
2020-02-11 18:15:30 +0900

    example use hard-source-webpack-plugin

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,13 +1,29 @@
 'use strict';
 
 const path = require('path');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
-const entries = {
-  index: path.resolve(__dirname, 'src', 'index.js')
-};
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const plugins = [];
+if (NODE_ENV !== 'production') {
+  plugins.push(
+    new HardSourceWebpackPlugin({
+      cacheDirectory: path.resolve(__dirname, 'cache/hard-source/js/[confighash]'),
+      cachePrune: {
+        // cache must be at least 2weeks old.
+        maxAge: 14 * 24 * 60 * 60 * 1000,
+        // cache must be at least 100MB big in bytes.
+        sizeThreshold: 100 * 1024 * 1024
+      }
+    })
+  );
+}
 
 module.exports = {
-  entry: entries,
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js')
+  },
 
   output: {
     filename: '[name].js',
@@ -21,6 +37,8 @@ module.exports = {
       path.resolve(__dirname, 'node_modules')
     ]
   },
+
+  plugins,
 
   module: {
     rules: [
